7e112f0b125319c52a0250e4f9009dd5dd34b4b3
welcomeof <socceryuuki0924@gmail.com>
2025-05-22 12:16:52 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,9 @@
+import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-import os
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
-# Herokuなどの環境変数からトークンを取得
-TOKEN = os.getenv("8122823996:AAE15VaXUUtcutKGxI7T8cUX6hlb7GsrDf0")
-
-# /start コマンドの処理
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton("🌏 世界の動画はこちら", url="https://t.me/+A_k5WIBrwlNhMTE9")],
@@ -17,15 +14,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text(
+    message = (
+        "📍 Message:\n"
         "👋 ようこそ！\n\n"
         "このBotでは、世界各国の特別な動画コンテンツを無料でご覧いただけます🌍\n"
         "リアルな映像をすぐ体験してください！\n\n"
-        "👇 下のボタンからお好きな国を選んで、今すぐチェック！",
-        reply_markup=reply_markup
+        "👇 下のボタンからお好きな国を選んで、今すぐチェック！\n\n"
+        "⬇️ Inline buttons:"
     )
 
-# アプリケーションの起動
+    await update.message.reply_text(message, reply_markup=reply_markup)
+
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
